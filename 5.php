64165388422a5d01e0d9e8a86df47476070be938
thilinitd <thilini.td@gmail.com>
2019-03-13 21:05:13 +0530

    Numbers2

---
+++ 5.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+<title>numbers2</title>
+</head>
+<body>
+<?php
+echo "Absolute value:".abs(-300);
+echo "<br/>";
+
+echo "power value:".pow(2,5);
+echo "<br/>";
+
+echo "random number:".rand();
+echo "<br/>";
+
+echo "random number within a range:".rand(30,49);
+echo "<br/>";
+
+echo "reminder:".fmod(27,5);
+echo "<br/>";
+
+echo"square root:".sqrt(265);
+echo "<br/>";
+
+?>
+</body>
+</html>
